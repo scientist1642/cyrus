0213f405324eed2cb5df9b469edb17f09b5d2d9d
Zura Isakadze <z.isakadze@gmail.com>
2013-10-30 19:34:59 +0400

    Customized admin panel

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
-admin.site.register(Page)
+class PageInline(admin.TabularInline):
+    model = Page
+    extra = 3
+
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+            (None,            {'fields' : ['name']}),
+            ('Rating info',   {'fields' : ['views', 'likes'], 'classes' : ['collapse']})
+    ]
+    inlines = [PageInline]
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('category', 'title', 'url')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
